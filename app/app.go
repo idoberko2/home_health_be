fc47d5051166fb807b749772cf3ab0c2bae01abe
Ido Berkovich <idoberko2@gmail.com>
2022-02-16 17:42:07 +0200

    engine config

--- app/app.go
+++ app/app.go
@@ -2,10 +2,12 @@ package app
 
 import (
 	"context"
+	"os"
 
 	"github.com/idoberko2/home_health_be/engine"
 	"github.com/idoberko2/home_health_be/notifier"
 
+	"github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/sync/errgroup"
 )
@@ -21,6 +23,14 @@ func New() App {
 type app struct{}
 
 func (a *app) Run() {
+	avoiddotenv := os.Getenv("HC_AVOID_DOTENV")
+	if avoiddotenv == "" {
+		err := godotenv.Load()
+		if err != nil {
+			log.WithError(err).Fatal("Error loading .env file")
+		}
+	}
+
 	ctx := context.Background()
 	g, grpCtx := errgroup.WithContext(ctx)
 	g.Go(getStartEngine(grpCtx))
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/idoberko2/home_health_be
 go 1.16
 
 require (
+	github.com/joho/godotenv v1.4.0
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.8.1
